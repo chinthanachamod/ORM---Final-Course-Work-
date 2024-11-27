0d6a00dd48706a0679ab1ce5a8f88d2eb2b6201e
chinthanachamod <chinthanachamod50@gmail.com>
2024-11-27 11:47:06 +0530

    Commited

--- src/main/java/org/example/DAO/CrudDAO.java
+++ src/main/java/org/example/DAO/CrudDAO.java
@@ -3,7 +3,7 @@ package org.example.DAO;
 import java.sql.SQLException;
 import java.util.List;
 
-public interface CrudDAO<T> {
+public interface CrudDAO<T> extends SuperDAO {
     public boolean save(T entity) throws Exception;
     public boolean update(T entity) throws Exception;
     public boolean delete(String ID) throws Exception;
